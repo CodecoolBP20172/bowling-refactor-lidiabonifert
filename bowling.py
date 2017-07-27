2344d69233279d5043d04e87ed9fa55158cd2966
Lidia Bonifert <bonifert.lidia@gmail.com>
2017-07-27 16:42:03 +0200

    Name changes

--- bowling.py
+++ bowling.py
@@ -1,31 +1,31 @@
 def score(game):
     result = 0
-    frame = 1
+    frame_index = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+    for frame in range(len(game)):
+        if game[frame] == '/':
+            result += 10 - last_roll
         else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+            result += get_value(game[frame])
+        if frame_index < 10 and get_value(game[frame]) == 10:
+            if game[frame] == '/':
+                result += get_value(game[frame+1])
+            elif game[frame] == 'X' or game[frame] == 'x':
+                result += get_value(game[frame+1])
+                if game[frame+2] == '/':
+                    result += 10 - get_value(game[frame+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result += get_value(game[frame+2])
+        last_roll = get_value(game[frame])
         if not in_first_half:
-            frame += 1
+            frame_index += 1
         if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[frame] == 'X' or game[frame] == 'x':
             in_first_half = True
-            frame += 1
+            frame_index += 1
     return result
 
 
