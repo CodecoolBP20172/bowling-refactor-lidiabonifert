7299e71f65b42c564c3063451d15407c6f6e59d6
Lidia Bonifert <bonifert.lidia@gmail.com>
2017-07-28 18:05:11 +0200

    is_strike, is_spare functions impl

--- bowling.py
+++ bowling.py
@@ -3,16 +3,18 @@ def score(game):
     frame_index = 1
     in_first_half = True
     for frame in range(len(game)):
-        if game[frame] == '/':
+        if is_spare(game[frame]):
             result += 10 - last_roll
         else:
             result += get_value(game[frame])
+
         if frame_index < 10 and get_value(game[frame]) == 10:
-            if game[frame] == '/':
+            if is_spare(game[frame]):
                 result += get_value(game[frame+1])
-            elif game[frame] in 'Xx':
+            elif is_strike(game[frame]):
                 result += get_value(game[frame+1])
-                if game[frame+2] == '/':
+
+                if is_spare(game[frame+2]):
                     result += 10 - get_value(game[frame+1])
                 else:
                     result += get_value(game[frame+2])
@@ -23,18 +25,24 @@ def score(game):
             in_first_half = False
         else:
             in_first_half = True
-        if game[frame] in 'Xx':
+        if is_strike(game[frame]):
             in_first_half = True
             frame_index += 1
     return result
 
 
+def is_strike(frame):
+    return frame in 'Xx'
+
+
+def is_spare(frame):
+    return frame == '/'
+
+
 def get_value(char):
     if char in '123456789':
         return int(char)
-    elif char in 'Xx/':
+    elif is_strike(char) or is_spare(char):
         return 10
     elif char == '-':
         return 0
-    else:
-        raise ValueError()
